7cc69b23df563a0b4a1890dce37557701b96ad75
Jiawen Huang <jhuang448@gatech.edu>
2018-11-24 12:04:49 -0500

    audioDelay.m added

---
+++ audioDelay.m
@@ -0,0 +1,15 @@
+function x_delay = audioDelay(x, fs, delay, gain)
+% x: audio signal
+% fs: sampling rate
+% delay: delay time, in sample, default 6 (seconds) * fs
+% gain: feedback rate, default 0.75
+
+delay_num = ceil(log(0.01)/log(gain));
+orig_len = size(x, 1);
+x_delay = zeros(delay_num * delay + orig_len, 2);
+x_delay(1:orig_len, :) = x;
+for i = 1:delay_num
+    x_delay(i*delay+1:i*delay+orig_len, :) = x_delay(i*delay+1:i*delay+orig_len, :) + (gain^i) * x;
+end
+
+sound(x_delay, fs);
